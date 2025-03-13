29dd63e792252acfcd04318573e81391d2561a5f
harshita <harshita@logicrays.com>
2025-03-13 11:27:47 +0530

    13mar

--- react-basics/src/components/classcomp/LifeCycleMethods.jsx
+++ react-basics/src/components/classcomp/LifeCycleMethods.jsx
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 
 export default class LifeCycleMethods extends Component {
+  constructor(props) {
+    super(props)
+    console.log("constructor called")
+    this.state = { username:"Harshita",count:0}
+    this.timer = ''
+  }
+  static getDerivedStateFromProps(props,state){
+    console.log("getDerivedStateFromProps called",state)
+    return {state}
+  }
+  componentDidMount(){
+    console.log("componentDidMount called")
+    this.timer = setInterval(()=>{
+      console.log("setinterval called")
+      this.setState((prev)=>({count:prev.count+1}))
+    }, 2000)
+  }
+
+  shouldComponentUpdate(){
+    console.log("shouldComponentUpdate called")
+    return true
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate called")
+  }
+  componentWillUnmount(){
+    console.log("componentWillUnmount called")
+    clearInterval(this.timer)
+  }
   render() {
+    console.log("render called")
     return (
       <div>
-        
+        <h1>{this.state.username}</h1>
+        <button
+          type="button"
+          className="btn btn-primary" 
+          onClick={()=>{ console.log("change username called"); 
+            this.setState({username:"Ram"})}}
+        >
+          Button
+        </button>  
+
+        <h1>{this.state.count}</h1>
       </div>
     )
   }
--- react-basics/src/components/classcomp/Purecompdemo.jsx
+++ react-basics/src/components/classcomp/Purecompdemo.jsx
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, PureComponent } from 'react'
 
-export default class Purecompdemo extends Component {
+export default class Purecompdemo extends PureComponent {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       username:"harshita"
+    }
+  }
+  
   render() {
+    console.log("render called")
     return (
       <div>
-        
+         <button
+          type="button"
+          className="btn btn-primary" 
+          onClick={()=>{ console.log("change username called"); 
+            this.setState({username:"Ram"})}}
+        >
+          Button
+        </button>  
+        <h1>{this.state.username}</h1>
       </div>
     )
   }
